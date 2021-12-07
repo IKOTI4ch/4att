4c08baf34f0d031098920da6b44baed1343fe267
KOTI4 <kyropato4ka74d@gmail.com>
2021-12-07 21:25:11 +0500

    redacted login page with css

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,36 +1,39 @@
-import React, {useContext} from 'react';
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import {authRoutes, notAuthRoutes} from "../routes";
+import React, { useContext } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { authRoutes, notAuthRoutes } from "../routes";
 import Home from "./Home";
-import {IsAuthContext} from "../context";
+import { IsAuthContext } from "../context";
 
 const Routing = () => {
-    const {isAuth} = useContext(IsAuthContext)
-    return (
-            <BrowserRouter>
-                {
-                    isAuth
-                        ? <Routes>
-                            <Route path='/' element={<Home/>}/>
-                            {
-                                authRoutes
-                                    .map(route =>
-                                        <Route key={route.path} path={route.path} element={<route.element/>}/>
-                                    )
-                            }
-                            <Route path='*' element={<Navigate to='/'/>}/>
-                        </Routes>
-                        : <Routes>
-                            {
-                                notAuthRoutes
-                                    .map(route =>
-                                        <Route key={route.path} path={route.path} element={<route.element/>}/>
-                                    )
-                            }
-                            <Route path='*' element={<Navigate to='/login'/>}/>
-                        </Routes>}
-            </BrowserRouter>
-    );
+  const { isAuth } = useContext(IsAuthContext);
+  return (
+    <BrowserRouter>
+      {isAuth ? (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {authRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.element />}
+            />
+          ))}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      ) : (
+        <Routes>
+          {notAuthRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.element />}
+            />
+          ))}
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      )}
+    </BrowserRouter>
+  );
 };
 
 export default Routing;
